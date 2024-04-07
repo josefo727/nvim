940de6178104296f30fb5c019e4fc6823974b6d4
José Rafael Gutierrez <josefo727@gmail.com>
2024-04-07 14:48:26 -0500

    feat: initial configuration for working with PHP and JS

---
+++ lua/plugins/laravel.lua
@@ -0,0 +1,16 @@
+return {
+  "adalessa/laravel.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "tpope/vim-dotenv",
+    "MunifTanjim/nui.nvim",
+    "nvimtools/none-ls.nvim",
+  },
+  cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+  keys = {
+    { "<leader>la", ":Laravel artisan<cr>" },
+    { "<leader>lr", ":Laravel routes<cr>" },
+  },
+  event = { "VeryLazy" },
+  config = true,
+}
